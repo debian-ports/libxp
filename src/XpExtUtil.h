15ec6d1d0bb8c4cb24a190ed34e63312a0623670
Alan Coopersmith <alan.coopersmith@oracle.com>
2013-05-03 22:30:36 -0700

    Use _XEatDataWords to avoid overflow of rep.length bit shifting
    rep.length is a CARD32, so rep.length << 2 could overflow in 32-bit builds
    
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- src/XpExtUtil.h
+++ src/XpExtUtil.h
@@ -48,6 +48,20 @@ extern char *_xpstrdup(
     const char * /* str */
 );
 
+#ifndef HAVE__XEATDATAWORDS
+#include <X11/Xmd.h>  /* for LONG64 on 64-bit platforms */
+#include <limits.h>
+
+static inline void _XEatDataWords(Display *dpy, unsigned long n)
+{
+# ifndef LONG64
+    if (n >= (ULONG_MAX >> 2))
+        _XIOError(dpy);
+# endif
+    _XEatData (dpy, n << 2);
+}
+#endif
+
 _XFUNCPROTOEND
 
 #endif /* _XPEXTUTIL_H */
